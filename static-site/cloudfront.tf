13154f447cdccf82b52b2c4f54f9a16279b81b29
Nam Ngo <namhngo010203@gmail.com>
2026-08-03 14:02:46 -0400

    fix(static-site): disable compression on no-cache policy

--- static-site/cloudfront.tf
+++ static-site/cloudfront.tf
@@ -51,8 +51,8 @@ resource "aws_cloudfront_cache_policy" "no_cache" {
       query_string_behavior = "none"
     }
 
-    enable_accept_encoding_brotli = true
-    enable_accept_encoding_gzip   = true
+    enable_accept_encoding_brotli = false
+    enable_accept_encoding_gzip   = false
   }
 }
 
